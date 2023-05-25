d19298d440730c69045c64e69979e62ea057758a
Kevinmrozek <kmrozek0859@gmail.com>
2023-05-25 08:25:19 -0500

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-let main = document.querySelector('main#main');
+/*let main = document.querySelector('main#main');
 main.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id', 'victory');
 newHeader.textContent = `Kevin is the champion`;
 
-document.body.appendChild(newHeader);
+document.body.appendChild(newHeader);*/
+
+
+
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
