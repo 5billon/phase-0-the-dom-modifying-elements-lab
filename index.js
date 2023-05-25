f1656e45c47fbf6fb7e0e615b902769243900ada
Alexis <alexisbou16@gmail.com>
2023-05-25 09:34:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-/*let main = document.querySelector('main#main');
-main.remove();
-
-let newHeader = document.createElement('h1');
-newHeader.setAttribute('id', 'victory');
-newHeader.textContent = `Kevin is the champion`;
-
-document.body.appendChild(newHeader);*/
-
-
-
-
 document.getElementById('main').remove();
 
 const newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = 'Alexis is the champion';
